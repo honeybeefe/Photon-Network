a931a9c7f83917a212e2c4afa0ce30c4cd9fb7d1
honeybeefe <44984498a@gmail.com>
2024-10-29 19:45:41 +0900

    2024.10.29 네비메쉬 에이전트

--- Photon Network Game/Assets/Scripts/DialogManager.cs
+++ Photon Network Game/Assets/Scripts/DialogManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class DialogManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] InputField inputField;
+    [SerializeField] ScrollRect scrollRect;
     [SerializeField] Transform parentTransform;
     
     
@@ -18,10 +19,13 @@ public class DialogManager : MonoBehaviourPunCallbacks
 
             //inputField에 있는 텍스트를 가져온다
             //(string 변수) <- Photon 닉네임 + : + InputField로 설정한 문자열
-            string talk = photonView.Owner.NickName + ":" + inputField.text;
+            string talk = PhotonNetwork.LocalPlayer.NickName + ":" + inputField.text;
 
             //RPC Targer.All : 현재 룸에 있는 모든 클라이언트에게 Talk() 함수를 실행하라는 명령 전달
             photonView.RPC("Talk", RpcTarget.All, talk);
+
+            //스크롤의 위치를 초기화
+            scrollRect.verticalNormalizedPosition = 0.0f;
         }
     }
 
@@ -36,6 +40,10 @@ public class DialogManager : MonoBehaviourPunCallbacks
         talk.transform.SetParent(parentTransform);
         //채팅을 입력한 후에도 이어서 입력할 수 있도록 설정
         inputField.ActivateInputField();
+
+        //스크롤의 위치를 초기화
+        scrollRect.verticalNormalizedPosition = 0.0f;
+
         //inputField의 텍스트를 초기화
         inputField.text = "";
     }
